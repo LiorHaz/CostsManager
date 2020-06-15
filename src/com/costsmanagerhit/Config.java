cfe71119899ab6eeb18a4a50ee8479f4b6a2a227
lihaz <liorhaz1310@gmail.com>
2020-06-15 12:37:27 +0300

    Code styling Checked successfully

--- src/com/costsmanagerhit/Config.java
+++ src/com/costsmanagerhit/Config.java
@@ -1,7 +1,7 @@
 package com.costsmanagerhit;
 
 /**
- *Holds the string which represents the the package name with the controller for router servlet's uses
+ *Holds the string which represents the package name with the controller for router servlet's uses
  */
 public class Config {
     public static final String CONTROLLERS_PACKAGE = "com.costsmanagerhit.controller";
